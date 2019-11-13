39f6029ee3ded8ffe8d0d295fb3774e4d540b84e
Václav Kubernát <kubervac@fit.cvut.cz>
2019-11-13 18:47:40 +0100

    Allow usage as a library
    The library returns middleware for Express.

---
+++ src/shortener.ts
@@ -0,0 +1,75 @@
+import * as Mongo from "mongodb";
+import express from "express";
+import md5 from "md5";
+import isWebUri from "valid-url";
+
+let database: Mongo.Collection;
+const resolveShortened = async (shortUrl: string): Promise<string> => {
+    let res = await database.find({ _id: shortUrl }).toArray();
+    if (res.length === 1) {
+        return res[0].url;
+    } else if (res.length === 0) {
+        throw new Error(`${shortUrl} is not shortened to anything.`);
+    } else {
+        throw new Error(`Logic error! Url ${shortUrl} is ambiguous!`);
+    }
+};
+
+const createShortened = async (url: string): Promise<string> => {
+    let shortUrl = md5(url).slice(0, 6);
+
+    if ((await database.find({ _id: shortUrl }).count()) === 0) {
+        await database.insertOne({ url, _id: shortUrl });
+    }
+    return shortUrl;
+};
+
+const isValidReqBody = (body: any): body is { url: string } => {
+    return typeof body === "object" && typeof body.url === "string";
+};
+
+
+const shortener = async (prefix = "") => {
+    const client = new Mongo.MongoClient("mongodb://localhost:27017/shortener", {
+        useUnifiedTopology: true
+    });
+    await client.connect();
+    database = client.db().collection("shortener");
+    const router = express.Router();
+    const options = {
+        dotfiles: "ignore"
+    };
+
+    router.use(express.static("dist", options));
+    router.use(express.json());
+
+    router.get(prefix + "/:link", async (req, response) => {
+        let shortUrl = req.params["link"];
+        console.log(`GET /${shortUrl}`);
+        try {
+            let resolved = await resolveShortened(shortUrl);
+            console.log(`${shortUrl} is resolved to "${resolved}"`);
+            response.redirect(resolved);
+        } catch (err) {
+            response.send(err.message);
+        }
+    });
+
+    router.post(prefix + "/", async (req, response) => {
+        console.log(`POST /, body:`, req.body);
+        if (isValidReqBody(req.body)) {
+            if (!isWebUri.isWebUri(req.body.url)) {
+                response.json({ ok: false, error: `"${req.body.url}" is not a valid URL by my standards.` });
+                return;
+            }
+            let shortUrl = await createShortened(req.body.url);
+            response.json({ ok: true, ...req.body, short: shortUrl });
+        } else {
+            response.json({ ok: false, error: "Invalid request." });
+        }
+    });
+
+    return router;
+}
+
+export default shortener;
